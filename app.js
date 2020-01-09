33543051462b4d47017935341dfa2d32adf9ae4a
Tyler C Smith <tylercsmith94@gmail.com>
2020-01-09 09:17:04 +0700

    Added init function

--- app.js
+++ app.js
@@ -1,31 +1,76 @@
+//BUDGET CONTROLLER
 var budgetController = (function() {
-        var x = 23;
 
-        var add = function(a) {
-            return x + a;
-        }
+        //some code
 
-        return {
-            publicTest: function(b) {
-                return add(b);
-            }
-        }
 })();
 
+
+//UI CONTROLLER
 var UIController = (function(){
 
-        // Some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will be inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }; 
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 })();
 
+
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-        var z = budgetCtrl.publicTest(5);
+    var setupEventListeners = function () {
 
-        return {
-            anotherPublic: function() {
-                console.log(z);
-            }
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+          if (event.keyCode === 13 || event.which === 13){
+              ctrlAddItem();
+          } 
+    
+        });
+    };
+
+    
+
+    var ctrlAddItem = function(){
+        //get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
+        //add the item to the budget controller
+
+        // add the item to the ui
+
+        // calculate the budget
+
+        //display the budget on the ui
+    };
+    
+    return {
+        init: function(){
+            console.log('application started!');
+            setupEventListeners();
         }
+    };
 
 })(budgetController, UIController);
+
+controller.init();
